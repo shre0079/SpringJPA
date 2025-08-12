89ad32cda298ad656fa57ea25304470f303cc3df
Shreyash <shreyashshree0079@gmail.com>
2025-08-12 23:11:14 +0530

    method updates

--- src/main/java/com/shre/spring_data_jpa/SpringDataJpaApplication.java
+++ src/main/java/com/shre/spring_data_jpa/SpringDataJpaApplication.java
@@ -18,9 +18,21 @@ public class SpringDataJpaApplication {
         Student s2 = context.getBean(Student.class);
         Student s3 = context.getBean(Student.class);
 
-        s1.setRollNo(101);
-        s1.setName("Navin");
-        s1.setMarks(75);
+//        s1.setRollNo(101);
+//        s1.setName("Navin");
+//        s1.setMarks(75);
+//
+//        s2.setRollNo(102);
+//        s2.setName("Kiran");
+//        s2.setMarks(75);
+//
+//        s3.setRollNo(103);
+//        s3.setName("Harsh");
+//        s3.setMarks(75);
+//
+//        studentRepository.save(s1);
+//        studentRepository.save(s2);
+//        studentRepository.save(s3);
 
         s2.setRollNo(102);
         s2.setName("Kiran");
